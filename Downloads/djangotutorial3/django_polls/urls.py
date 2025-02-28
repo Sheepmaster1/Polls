ff1cdb0803baff28b7efec977732a4176226885e
Jaime <jaime.vasco500@gmail.com>
2025-02-28 15:01:38 +0000

    Initial commit

---
+++ Downloads/djangotutorial3/django_polls/urls.py
@@ -0,0 +1,33 @@
+"""
+URL configuration for mysite project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from . import views
+
+app_name = "polls"  # This is for namespace, so you can refer to URLs as 'polls:index', etc.
+urlpatterns = [
+    # URL for the index page (list of questions)
+    path("", views.IndexView.as_view(), name="index"),
+
+    # URL for a specific question detail page
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+
+    # URL for displaying results of a specific question
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+
+    # URL for submitting a vote for a specific question
+    path("<int:question_id>/vote/", views.vote, name="vote"),
+]
